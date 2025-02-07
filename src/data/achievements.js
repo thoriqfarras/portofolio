b8392911ae4297b3ae596715c662b7923345cb39
thoriqWslLaptop <thoriqibrahimfarras@gmail.com>
2025-02-07 17:17:15 +0700

    restructure blog content

--- src/data/achievements.js
+++ src/data/achievements.js
@@ -2,12 +2,12 @@ export const achievements = [
   {
     title: 'Gemastik 2024',
     description: 'Country Top 20 thanks our AI-based flashcard study app.',
-    blogUrl: '/content/blog/gemastik/gemastik.md',
+    blogUrl: '/blog/gemastik/gemastik.md',
   },
   {
     title: 'GDSC Solution Challenge 2024',
     description:
       'Global Top 100 thanks to our on-demand emergency services app.',
-    blogUrl: '/content/blog/gsc/gsc.md',
+    blogUrl: '/blog/gsc/gsc.md',
   },
 ];
--- src/data/work.js
+++ src/data/work.js
@@ -2,11 +2,11 @@ export const works = [
   {
     title: 'Repetit',
     description: 'A study assistant web app where flashcards meets AI.',
-    blogUrl: '/content/blog/repetit/repetit.md',
+    blogUrl: '/blog/repetit/repetit.md',
   },
   {
     title: 'Si Rene',
     description: 'On demand emergency services for mobile.',
-    blogUrl: '/content/blog/si-rene/si-rene.md',
+    blogUrl: '/blog/si-rene/si-rene.md',
   },
 ];
